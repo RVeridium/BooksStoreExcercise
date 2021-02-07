697ca30d59e5ede05b3b712261842ea074fc838f
RVeridium <riitta.icer@gmail.com>
2021-02-07 22:01:02 +0200

    editings

--- src/main/java/fi/palvelin/bookstore/web/BookController.java
+++ src/main/java/fi/palvelin/bookstore/web/BookController.java
@@ -3,6 +3,7 @@ package fi.palvelin.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,10 +40,19 @@ public class BookController {
 	    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 	    	repo.deleteById(bookId);
 	        return "redirect:../booklist";
-	    }   
-
-	
-	
-	
+	    }
+	 
+	 @RequestMapping(value = "edit/{id}", method = RequestMethod.GET)
+	 	public String editBook(@PathVariable("id") long bookId, Model model) {
+		 model.addAttribute("book", repo.findById(bookId)); 
+		 return "editbook"; 
+	 }
+	 
+	 @RequestMapping(value = "edit/update/{id}", method = RequestMethod.POST)
+	    public String update(@PathVariable("id") long bookId, @ModelAttribute("book") Book book, Model model) {
+		 	repo.save(book); 
+		 	model.addAttribute("books", repo.findAll()); 
+	        return "redirect:/booklist";
+	    }
 
 }
